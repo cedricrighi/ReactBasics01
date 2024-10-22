cd32c04ad880e140f3dd6b89f0aba5aab46864b2
Gamberge <cedric.righi@gmail.com>
2024-10-22 14:31:04 +0200

    make state.1

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -8,16 +9,42 @@ const pokemonList = [
 			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	},
 	{
+		name: "charmander",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
 		name: "mew",
 	},
 ];
 
 function App() {
-  return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
-  );
+	const [PokemonIndex, setPokemonIndex] = useState(0);
+	return (
+		<div>
+			{PokemonIndex > 0 ? (
+				<button onClick={() => setPokemonIndex(PokemonIndex - 1)}>
+					Précédent
+				</button>
+			) : null}
+			<PokemonCard pokemon={pokemonList[PokemonIndex]} />
+			{PokemonIndex < pokemonList.length - 1 ? (
+				<button onClick={() => setPokemonIndex(PokemonIndex + 1)}>
+					Suivant
+				</button>
+			) : null}
+		</div>
+	);
 }
 
 export default App;
